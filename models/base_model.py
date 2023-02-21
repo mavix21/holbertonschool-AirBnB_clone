5a79c8fcc2a39ce045e576e1b453f30cb64a26dd
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-02-21 15:34:08 -0800

    feat: initial commit

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Define a class BaseModel to support other classes.
+"""
+
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    """Represent support to other classes.
+    """
+
+    def init(self, id="", created_at=None, updated_at=None):
+        """Constructor new instances of class.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def str(self):
+        """String representation of the instances.
+        """
+        str = "[{}] ({}) {}".format(self.class.name, self.id, self.dict)
+        return(str)
+
+    def save(self):
+        """Updates the public instance attributes.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values.
+        """
+        n_dict = self.dict.copy()
+        if "created_at" in n_dict:
+            n_dict["created_at"] = n_dict["created_at"].isoformat()
+        if "updated_at" in n_dict:
+            n_dict["updated_at"] = n_dict["updated_at"].isoformat()
+        n_dict["class"] = self.class.name
+        return n_dict
+
