8bf80e520273dd3c7fc75daee82d8608fa4b1c04
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-02-21 16:28:16 -0800

    fix: correct init method declaration

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel:
     """Represent support to other classes.
     """
 
-    def init(self, id=None, created_at=None, updated_at=None):
+    def __init__(self):
         """Constructor new instances of class.
         """
         self.id = str(uuid.uuid4())
