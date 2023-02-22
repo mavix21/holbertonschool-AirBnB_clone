b053320b264188d3a204adb9a01605193548facf
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-02-21 22:59:12 -0800

    feat: unittesting for base_model.py

---
+++ tests/test_base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Test for base_model module"""
+
+import unittest
+import models
+
+class_path = models.base_model.BaseModel
+
+
+class Test_Base_Model(unittest.TestCase):
+    """Test class for testing base_model module"""
+
+    def test_base_model_doc(self):
+        """Testing base_model module documentation"""
+        self.assertIsNotNone(models.base_model.__doc__)
+
+    def test_BaseModel_class_doc(self):
+        """Testing BaseModel class documentation"""
+        self.assertIsNotNone(class_path.__doc__)
+
+    def test_save_method_doc(self):
+        """Testing save method documentation"""
+        self.assertIsNotNone(class_path.save.__doc__)
+
+    def test_to_dict_method_doc(self):
+        """Testing to_dict method documentation"""
+        self.assertIsNotNone(class_path.to_dict.__doc__)
+
+    def test_to_dict_method(self):
+        """Testing to_dict method return type"""
+        self.assertIs(type(class_path.to_dict()), dict)
+
+    def test_init_doc(self):
+        """Testing __init__ method documentation"""
+        self.assertIsNotNone(class_path.__init__.__doc__)
+
+    def test_id_value(self):
+        """Testing id attribute value"""
+        self.assertIs(type(class_path.self.id), str)
+
+
+if __name__ == '__main__':
+    unittest.main
