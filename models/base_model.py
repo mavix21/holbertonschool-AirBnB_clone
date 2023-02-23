a21336782d7acfddd84e626282aafbad1fc5e06d
mavix21 <marcevizcarra2109@hotmail.com>
2023-02-23 01:37:29 -0500

    Feat(Add): File storage

--- models/base_model.py
+++ models/base_model.py
@@ -4,15 +4,25 @@
 
 import uuid
 from datetime import datetime
+from . import storage
 
 
 class BaseModel:
-    """Represent support to other classes.
-    """
+    """ Represent support for other classes. """
 
     def __init__(self, *args, **kwargs):
-        """Constructor new instances of class.
+        """ Constructor method
+
+        Parameters:
+            args (tuple):
+                Not intented to be used. If passed, it will be skiped
+
+            kwargs (dict):
+                The keys of this dicionary are the name attributes and
+                the values are the values of each attribute
+
         """
+
         if kwargs:
             for key in kwargs:
                 if key != '__class__':
@@ -30,26 +40,30 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
-        """String representation of the instances.
-        """
+        """ String representation of the instances """
+
         str = "[{}] ({}) {}".format(type(self).__name__,
                                     self.id, self.__dict__)
         return (str)
 
     def save(self):
-        """Updates the public instance attributes.
-        """
+        """ Updates the public instance attributes """
+
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values.
-        """
+        """ Returns a dictionary containing all keys/values. """
+
         n_dict = self.__dict__.copy()
-        if "created_at" in n_dict:
+        if hasattr(self, "created_at"):
             n_dict["created_at"] = n_dict["created_at"].isoformat()
-        if "updated_at" in n_dict:
+
+        if hasattr(self, "updated_at"):
             n_dict["updated_at"] = n_dict["updated_at"].isoformat()
         n_dict["__class__"] = self.__class__.__name__
+
         return (n_dict)
