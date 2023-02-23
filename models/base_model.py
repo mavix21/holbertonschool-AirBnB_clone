eb898a73ddc3d47ea2bfeebfa6c0e4c47a704d8c
mavix21 <marcevizcarra2109@hotmail.com>
2023-02-22 22:01:53 -0500

    Fix: pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -17,12 +17,15 @@ class BaseModel:
             for key in kwargs:
                 if key != '__class__':
                     setattr(self, key, kwargs[key])
-                if hasattr(self, "created_at") and type(self.created_at) is str:
+
+                if hasattr(self, "created_at") and \
+                        type(self.created_at) is str:
                     self.created_at = datetime.strptime(self.created_at,
-                                       '%Y-%m-%dT%H:%M:%S.%f')
-                if hasattr(self, "updated_at") and type(self.updated_at) is str:
-                    self.updated_at = (datetime.strptime(self.updated_at,
-                                       '%Y-%m-%dT%H:%M:%S.%f')
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                if hasattr(self, "updated_at") and \
+                        type(self.updated_at) is str:
+                    self.updated_at = datetime.strptime(self.updated_at,
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
