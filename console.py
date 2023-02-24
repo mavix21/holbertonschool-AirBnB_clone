0637b74dc248e754b7fd111a425d74b83666f085
johnNaduer <naduer789@gmail.com>
2023-02-23 21:13:36 -0500

    fix: standard style PEP8

--- console.py
+++ console.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-
+""" created class  HBNBCommand """
 import cmd
+
 
 class HBNBCommand(cmd.Cmd):
     """ the entry point of the command interpreter """
     prompt = '(hbnb)'
     
     def	do_quit(self, line):
+        """ exit program """
         return True
 
     def do_EOF(self, line):
+        """ exit program """
         return True
 
     def emptyline(self):
