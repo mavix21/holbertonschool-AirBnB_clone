e3a867803d02ed8977d86197261dbc0f2f535b82
johnNaduer <naduer789@gmail.com>
2023-02-23 21:11:48 -0500

    feat: add method emptyline

--- console.py
+++ console.py
@@ -4,4 +4,17 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """ the entry point of the command interpreter """
+    prompt = '(hbnb)'
     
+    def	do_quit(self, line):
+        return True
+
+    def do_EOF(self, line):
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty line"""
+        print("")
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()    
