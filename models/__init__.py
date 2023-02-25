0a73dd84a2ee7489087b160aacdf560d73347d48
mavix21 <marcevizcarra2109@hotmail.com>
2023-02-25 17:56:53 -0500

    Fix: Remove whitespace

--- models/__init__.py
+++ models/__init__.py
@@ -3,13 +3,13 @@
     This module creates a unique FileStorage instance and reloads the objects
 """
 
-from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
+from .engine.file_storage import FileStorage
+from .base_model import BaseModel
+from .user import User
 
 
 classes = {
-    "BaseModel": BaseModel, 
+    "BaseModel": BaseModel,
     "User": User
 }
 
