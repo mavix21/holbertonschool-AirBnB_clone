3fcde27e6083a51f3db3f735067614876ba5d1fa
johnNaduer <naduer789@gmail.com>
2023-02-25 17:01:52 -0500

    feat: add class User that inherits from BaseModel

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module defines the User class """
+
+
+from . import BaseModel
+
+
+class User(BaseModel)
+    """ A class representing a user """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
