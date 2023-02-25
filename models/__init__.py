fc2010afb287c13f270fcbc2fa12def279f87924
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-02-25 14:37:49 -0800

    feat: update classes dict

--- models/__init__.py
+++ models/__init__.py
@@ -7,7 +7,8 @@ from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
 classes = {
-    "BaseModel": BaseModel
+    "BaseModel": BaseModel, 
+    "User": User
 }
 
 storage = FileStorage()
