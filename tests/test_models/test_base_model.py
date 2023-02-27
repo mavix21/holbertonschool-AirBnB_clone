b5a390af77645e8be701acf4709a8b7bbc4a013d
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-02-26 20:54:44 -0800

    add: more test for instances

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,6 +5,7 @@ import unittest
 import models
 
 class_path = models.base_model.BaseModel
+instance = class_path()
 
 
 class Test_Base_Model(unittest.TestCase):
@@ -20,21 +21,35 @@ class Test_Base_Model(unittest.TestCase):
 
     def test_save_method_doc(self):
         """Testing save method documentation"""
-        self.assertIsNotNone(class_path.save.__doc__)
+        self.assertIsNotNone(instance.save.__doc__)
 
     def test_to_dict_method_doc(self):
         """Testing to_dict method documentation"""
-        self.assertIsNotNone(class_path.to_dict.__doc__)
+        self.assertIsNotNone(instance.to_dict.__doc__)
 
     def test_to_dict_method(self):
         """Testing to_dict method return type"""
-        dummy_obj = class_path()
-        self.assertIs(type(dummy_obj.to_dict()), dict)
+        self.assertIs(type(instance.to_dict()), dict)
 
     def test_id_value(self):
         """Testing id attribute value"""
-        dummy_obj = class_path()
-        self.assertIs(type(dummy_obj.id), str)
+        self.assertIs(type(instance.id), str)
+
+    def test_str_method_doc(self):
+        """Testing str method documentation"""
+        self.assertIsNotNone(instance.__str__.__doc__)
+
+    def test_str_method_(self):
+        """Testing str method documentation"""
+        self.assertIs(type(instance.__str__()), str)
+
+    def test_init_method_doc(self):
+        """Testing str method documentation"""
+        self.assertIsNotNone(instance.__init__.__doc__)
+
+    def test_init_method_(self):
+        """Testing str method documentation"""
+        self.assertIs(type(instance.id, str))
 
 
 if __name__ == '__main__':
