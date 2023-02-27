560ec3ce6d281b1191e0359ce5dfd559bfb42325
mavix21 <marcevizcarra2109@hotmail.com>
2023-02-27 02:08:24 -0500

    Feat: Improve parsing

--- utils/console_utils.py
+++ utils/console_utils.py
@@ -9,12 +9,26 @@ def parse_args(line):
 
     args = []
     quote_mode = False
+    brace_mode = False
+    brace_level = 0
     arg_start = 0
 
     for i, c in enumerate(line):
         if c == '"':
+            if brace_mode:
+                continue
             quote_mode = not quote_mode
-        elif not quote_mode and c.isspace():
+        elif c == '{':
+            if not quote_mode:
+                brace_mode = True
+            brace_level += 1
+        elif c == '}':
+            if not quote_mode:
+                brace_level -= 1
+
+            if brace_level == 0:
+                brace_mode = False
+        elif not quote_mode and not brace_mode and c.isspace():
             if arg_start < i:
                 args.append(line[arg_start:i])
             arg_start = i + 1
@@ -22,7 +36,19 @@ def parse_args(line):
     if arg_start < len(line):
         args.append(line[arg_start:])
 
-    return [arg.strip('"') for arg in args]
+    args = [arg.strip('"') for arg in args]
+    final_args = []
+    for arg in args:
+        if '{' in arg and '}' in arg:
+            start = arg.find('{')
+            end = arg.rfind('}')
+            final_args.extend(arg[:start].split())
+            final_args.append(arg[start:end+1])
+            final_args.extend(arg[end+1:].split())
+        else:
+            final_args.append(arg)
+
+    return final_args
 
 
 def validate_args(args, number_of_validations=2):
@@ -38,11 +64,11 @@ def validate_args(args, number_of_validations=2):
         print("** instance id missing **")
         return False
 
-    if number_of_validations >= 4 and len(args) == 1:
+    if number_of_validations >= 4 and len(args) == 2:
         print("** attribute name missing **")
         return False
 
-    if number_of_validations >= 5 and len(args) == 1:
+    if number_of_validations >= 5 and len(args) == 3:
         print("** value missing **")
         return False
 
