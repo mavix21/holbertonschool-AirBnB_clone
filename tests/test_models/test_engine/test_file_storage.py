aaab1a42ccb49a3ecfbc530672a32cdce7efcc7f
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-02-28 13:50:12 -0800

    fix: unmatched parenthesis

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -21,7 +21,7 @@ class TestFileStorage(unittest.TestCase):
 
     def test__file_path(self):
         """Test for class private attribute __file_path."""
-        self.assertIsNotNone(instance._FileStorage__objects))
+        self.assertIsNotNone(instance._FileStorage__objects)
 
     def test__objects(self):
         """Test for class private attribute __objects."""
