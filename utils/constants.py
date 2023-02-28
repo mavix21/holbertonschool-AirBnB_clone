74f1382c953b0f3a629bf358c9fae1e789de7b79
marcelo.vizcarra <marcelo.vizcarra@inventiva-solutions.com>
2023-02-28 17:35:15 -0500

    Feat(Add): constants module that defines variables to share across the project

---
+++ utils/constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+This module defines constant variables to use accross the project
+"""
+
+re_cmd = r'\w+\.\w+\((?:"[^"]*"|{[^{}]*}|\s*)(?:,\s*(?:"[^"]*"|{[^{}]*}))*\)'
+re_inside_parenthesis = r'\((.*?)\)'
